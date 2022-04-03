20ac52111a3c81a17bbc3f710ccadae6b64bda9f
Liat <liat100200@gmail.com>
2022-04-04 02:49:55 +0300

    fix api

--- backend/api/rest/index.js
+++ backend/api/rest/index.js
@@ -4,14 +4,16 @@ const { getAllSymbols, getSymbolsData } = require('../../service/bitfinex');
 
 const rest = express();
 
+rest.use(express.json());
+
 rest.get('/api/symbols', async(req, res) => {
     return getAllSymbols().then(data => res.json(data));
 });
 
-rest.get('/api/full_data', async(req, res) => {
-    const symbolsArr = req.query.symbols;
+rest.post('/api/get_full_data', async(req, res) => {
+    const symbolsArr = req.body.symbols;
     if (symbolsArr) {
-        getSymbolsData(JSON.parse(symbolsArr)).then((data) => res.json(data));
+        getSymbolsData(symbolsArr).then((data) => res.json(data));
     } else {
         return res.status(500);
     }
