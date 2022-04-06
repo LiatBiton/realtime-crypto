12e1f1291fbb047be7f16845bfb4756002665e01
Liat <liat100200@gmail.com>
2022-04-06 11:37:58 +0300

    modal scss

--- frontend/src/cmps/TransferModal/index.jsx
+++ frontend/src/cmps/TransferModal/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import "./index.scss";
 
 export function TransferModal({ symbol, closeModal }){
@@ -9,36 +10,50 @@ export function TransferModal({ symbol, closeModal }){
     const [rate, setRate] = useState('');
     const [alignment, setAlignment] = useState('buy');
 
+    const theme = createTheme({
+        palette: {
+          primary: {
+            main: '#ffffff',
+          },
+          secondary: {
+            main: '#ffffff',
+          },
+        },
+      });
+
     const onSaveTransfer = () => {
         console.log('symbol:', symbol, 'count:', count, 'rate:', rate)
         closeModal()
     }
 
     const handleChange = (event, newAlignment) => {
-        setAlignment(newAlignment);
-        
-      };
+        setAlignment(newAlignment);   
+    };
 
 
     return (
         <section className="modal-container">
             <div className="modal-content">
                 <button className="close-btn" onClick={closeModal}>x</button>
-                <h2 className="header-transfer-modal"><span className="pink">{symbol[0]}</span> has just crossed the limit!</h2>
+                <h2 className="header-transfer-modal"><span className="bold">{symbol[0]}</span> has just crossed the limit!</h2>
                 <h3>Current price is : {symbol[7]}</h3>
 
                 <section className="alerts">
                     <p>What action would you like to do?</p>
                     <div className='inputs'>
-                        <ToggleButtonGroup
-                        color='warning'
-                        value={alignment}
-                        exclusive
-                        onChange={handleChange}
-                        >
-                            <ToggleButton value="web">Buy</ToggleButton>
-                            <ToggleButton value="android">Sell</ToggleButton>
-                        </ToggleButtonGroup>
+                        <div className="toggle">
+                        <ThemeProvider theme={theme}>
+                            <ToggleButtonGroup
+                            color="secondary"
+                            value={alignment}
+                            exclusive
+                            onChange={handleChange}
+                            >
+                                <ToggleButton value="web">Buy</ToggleButton>
+                                <ToggleButton value="android">Sell</ToggleButton>
+                            </ToggleButtonGroup>
+                        </ThemeProvider>
+                        </div>
                         <div className="margin">
                             <label htmlFor="rate">Rate:</label>
                             <input className="input-transfer-modal" type="number" min="0" max="1" step="0.01" id="rate" name="rate" onChange={(ev)=>setCount(ev.target.value)}/>
